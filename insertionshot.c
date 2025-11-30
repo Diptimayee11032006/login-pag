feb4a0566bf3e7154ac963ec6f046d68d00c2458
Diptimayee11032006 <diptimayeesutar2006@gmail.com>
2025-11-30 22:58:56 +0530

    xyz

---
+++ insertionshot.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a[10], i, j, n, temp;
+
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements: ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 1; i < n; i++) {
+        temp = a[i];
+        j = i - 1;
+
+        while (j >= 0 && a[j] > temp) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = temp;
+    }
+ 
+    printf("\nSorted Array is: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
